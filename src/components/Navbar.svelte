6a5962398f87f145d8a9e7544caf617b0ad3b32a
Carlos Martinez <curlosmart@gmail.com>
2022-03-07 22:58:32 -0500

    Add navbar

---
+++ src/components/Navbar.svelte
@@ -0,0 +1,51 @@
+<script>
+</script>
+
+<div class="container">
+	<div>
+		<img src="/assets/images/logo.svg" alt="sunnyside logo" />
+	</div>
+
+	<div class="right">
+		<a href="/">About</a>
+		<a href="/">Services</a>
+		<a href="/">Projects</a>
+		<button type="button">CONTACT</button>
+	</div>
+</div>
+
+<style>
+	.container {
+		background-color: #4abdf2;
+		padding: 20px;
+		display: flex;
+		justify-content: space-between;
+		align-items: center;
+		color: #ffffff;
+	}
+
+	.right {
+		display: flex;
+		align-items: center;
+		gap: 20px;
+		color: #ffffff;
+	}
+
+	a {
+		text-decoration: none;
+		color: #ffffff;
+		letter-spacing: -0.13px;
+	}
+
+	button {
+		text-transform: uppercase;
+		font-family: Fraunces;
+		font-size: 15px;
+		font-weight: 700;
+		color: #24303e;
+		margin: 0;
+		border: none;
+		padding: 15px 20px;
+		border-radius: 30px;
+	}
+</style>
