bd48cec38982f209703b7159fb8de9e80dcd35d3
Carlos Martinez <curlosmart@gmail.com>
2022-03-07 22:58:26 -0500

    Setup project and assets

---
+++ src/App.svelte
@@ -0,0 +1,18 @@
+<script>
+	import Navbar from "./components/Navbar.svelte";
+	export let name;
+</script>
+
+<main>
+	<Navbar />
+</main>
+
+<style>
+	@import url("https://fonts.googleapis.com/css2?family=Barlow:wght@600&family=Fraunces:wght@700;900&display=swap");
+
+	main {
+		font-family: Barlow;
+		font-size: 18px;
+		font-weight: 600px;
+	}
+</style>
